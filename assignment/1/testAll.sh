1df95f1f593017b1dfca1cf7beec85395850474c
Fiona Lin <fiona_lindley@yahoo.com.au>
2016-12-24 00:51:32 +1100

    add valgrind command

--- assignment/1/testAll.sh
+++ assignment/1/testAll.sh
@@ -6,9 +6,10 @@ do
 	echo
 	echo $i#;
 	# cat  testfiles/test$i
-clang -Wall -Werror -g testcase/dry$i.c textbuffer.c -o dry$i
-./dry$i >result
-diff -iEBw testoutput/result$i result
+ clang -Wall -Werror -g testcase/dry$i.c textbuffer.c -o dry$i
+ ./dry$i >result
+ diff -iEBw testoutput/result$i result
+ valgrind --leak-check=full -v ./dry$i
 rm dry$i
 done
 
