7d3837771f15c61eac4ebd8b42b87a8b7e3038f5
FionaCLin <clin4614@uni.sydney.edu.au>
2016-12-27 18:39:58 +1100

    team work test case

--- lab03/lab03/BST.c
+++ lab03/lab03/BST.c
@@ -15,26 +15,26 @@ struct treeNode {
 
 
 static treelink createNode(TreeItem item){
-     treelink t = (treelink) malloc (sizeof (*t));
-     t->item = item;
-     t->left = NULL;
-     t->right = NULL;
-     return t;
+    treelink t = (treelink) malloc (sizeof (*t));
+    t->item = item;
+    t->left = NULL;
+    t->right = NULL;
+    return t;
 }
 
 TreeItem getItem(treelink t){
-     assert(t != NULL);
-     return t->item;
+    assert(t != NULL);
+    return t->item;
 }
 
 void printTreeNode (treelink t) {
-     if(t != NULL);
-     printf("%d ",t->item);
+    if(t != NULL);
+    printf("%d ",t->item);
 }
 
 void preorderTraversal (treelink tree, void (*visit) (treelink)) {
     if (tree == NULL) {
-       return;
+        return;
     }
     (*visit)(tree);
     preorderTraversal (tree->left,visit);
@@ -43,18 +43,18 @@ void preorderTraversal (treelink tree, void (*visit) (treelink)) {
 
 //This function inserts duplcates on the left
 treelink insertTreeNode (treelink tree, TreeItem item) {
-  if (tree == NULL) {
-      tree = createNode(item);
-      tree->item = item;
-      tree->left = NULL;
-      tree->right = NULL;
-   
-  } else if (tree->item < item) {
-      tree->right = insertTreeNode (tree->right, item);
-  } else {
-      tree->left = insertTreeNode (tree->left, item);
-  }
-  return tree;
+    if (tree == NULL) {
+        tree = createNode(item);
+        tree->item = item;
+        tree->left = NULL;
+        tree->right = NULL;
+
+    } else if (tree->item < item) {
+        tree->right = insertTreeNode (tree->right, item);
+    } else {
+        tree->left = insertTreeNode (tree->left, item);
+    }
+    return tree;
 }
 
 int size(treelink t){
@@ -81,49 +81,48 @@ treelink search(treelink t, TreeItem i){
 }
 
 int countLeaves(treelink tree) {
-   int count = 0;
-   //In-Order-Traversal
-   assert (tree != NULL);
-   
-   if (tree->left == NULL && tree->right == NULL){
-      count ++;
-   }
-   if (tree->left != NULL) {
-      count += countLeaves(tree->left);
-   }
-   if (tree->right != NULL) {
-      count += countLeaves(tree->right);
-   }
-   
-   return count;
+    int count = 0;
+    //In-Order-Traversal
+    if (tree != NULL) {  
+        if (tree->left == NULL && tree->right == NULL){
+            count ++;
+        }
+        if (tree->left != NULL) {
+            count += countLeaves(tree->left);
+        }
+        if (tree->right != NULL) {
+            count += countLeaves(tree->right);
+        }
+    }
+    return count;
 }
 
 treelink searchInsert(treelink t, TreeItem i) {
-  /* int isIn = 0;
-   int item = 0;
-   int nCount = 0;
-   if (t->item == i) {
-     isIn++;
-     nCount++; 
-   }
-   if (tree->left != NULL && isIn == 0) {
-      item = t->left->item;
-      nCount++;
-      isIn += searchInsert(t->left,item);
-   }
-   if (tree->right != NULL && isIn == 0) {
-      item = t->right->item;
-      nCount++;
-      isIn += searchInsert(t->right,item);
-   }
-   if (isIn == 0 && nCount != size(t)) {
-      return isIn;
-   }
-   else if (isIn == 0 && nCount == size(t)) {
-      insertTreeNode(t, i);
-      return t;
-   }*/
-   
+    /* int isIn = 0;
+       int item = 0;
+       int nCount = 0;
+       if (t->item == i) {
+       isIn++;
+       nCount++; 
+       }
+       if (tree->left != NULL && isIn == 0) {
+       item = t->left->item;
+       nCount++;
+       isIn += searchInsert(t->left,item);
+       }
+       if (tree->right != NULL && isIn == 0) {
+       item = t->right->item;
+       nCount++;
+       isIn += searchInsert(t->right,item);
+       }
+       if (isIn == 0 && nCount != size(t)) {
+       return isIn;
+       }
+       else if (isIn == 0 && nCount == size(t)) {
+       insertTreeNode(t, i);
+       return t;
+       }*/
+
     treelink result = NULL;
     if (t == NULL) {
         if (size(t) == 0){
@@ -131,8 +130,8 @@ treelink searchInsert(treelink t, TreeItem i) {
             result = t;
         }
         else {
-           result = insertTreeNode(t, i);
-         
+            result = insertTreeNode(t, i);
+
         }
     } else if( i < t->item ){
         result = searchInsert(t->left,i); 
@@ -143,24 +142,26 @@ treelink searchInsert(treelink t, TreeItem i) {
 }
 
 int countIf (treelink tree, int (*pred)(TreeItem)){
-   int count = 0;
-   assert(tree != NULL);
-   count += (*pred)(tree->item);
-   if (tree->left != NULL) {
-      count += countIf (tree->left, pred);
-   }
-   if (tree->right != NULL) {
-      count += countIf (tree->right, pred);
-   }
-   return count;
+    int count = 0;
+    if(tree != NULL) {
+
+        count += (*pred)(tree->item);
+        if (tree->left != NULL) {
+            count += countIf (tree->left, pred);
+        }
+        if (tree->right != NULL) {
+            count += countIf (tree->right, pred);
+        }
+    }
+    return count;
 }
 
 int isEven (TreeItem n){
-   return (n%2 == 0);
+    return (n%2 == 0);
 }
 int isOdd (TreeItem n){
-   return (n%2==1);
+    return (n%2==1);
 }
 int isNegative (TreeItem n){
-   return (n<0);
+    return (n<0);
 }
--- lab03/lab03/testBST.c
+++ lab03/lab03/testBST.c
@@ -3,39 +3,57 @@
 #include <assert.h>
 #include "BST.h"
 
-int main(int argc, char * argv []) {
-    i= 0;
+void testCountLeaves();
+void testCountIf();
 
+int main(int argc, char * argv []) {
+
+    testCountLeaves();
+    testCountIf();
+    testSearchInsert();
+
+    return EXIT_SUCCESS;
+}
+// Should also test that insert node works for when there's no elements in the tree
+
+void testCountLeaves() {
+    int i= 0;
+    int c;
+
+    treelink t = NULL;
     printf("\n## Test %d: 0 leaf tree\n", ++i);
-    
+    c = countLeaves(t);
+    assert(c == 0);
+    printf("%d leaves tree\t ## passed\n", c);
+
     printf("\n## Test %d: 1 leaf tree\n", ++i);
-    treelink t = insertTreeNode(NULL, 10);
+    t = insertTreeNode(NULL, 10);
     c = countLeaves(t);
     assert(c == 1);
-    printf("%d tree\t ## passed\n", c);
+    printf("%d leaves tree\t ## passed\n", c);
 
     printf("\n## Test %d: 2 leaf tree\n", ++i);
     insertTreeNode(t, 4);
     insertTreeNode(t, 12);
     c = countLeaves(t);
     assert(c == 2);
-    printf("%d tree\t ## passed\n", c);
+    printf("%d leaves tree\t ## passed\n", c);
 
     printf("\n## Test %d: 3 leaf tree\n", ++i);
     insertTreeNode(t, 2);
     insertTreeNode(t, 7);
     c = countLeaves(t);
     assert(c == 3);
-    printf("%d tree\t ## passed\n", c);
-    
+    printf("%d leaves tree\t ## passed\n", c);
+
     printf("\n## Test %d: 4 leaf tree\n", ++i);
     insertTreeNode(t, 11);
     insertTreeNode(t, 18);
     c = countLeaves(t);
     assert(c == 4);
-    printf("%d tree\t ## passed\n", c);
-    
-    
+    printf("%d leaves tree\t ## passed\n", c);
+
+
     printf("\n## Test %d: 7 leaf tree\n", ++i);
     insertTreeNode(t, 1);
     insertTreeNode(t, 3);
@@ -43,17 +61,173 @@ int main(int argc, char * argv []) {
     insertTreeNode(t, 8);
     insertTreeNode(t, 14);
     insertTreeNode(t, 26);
-    
+
     c = countLeaves(t);
     assert(c == 7);
-    
-    
-    
-    printf("%d tree\t ## passed\n", c);
-    
-
-
-    return EXIT_SUCCESS;
+    printf("%d leaves tree\t ## passed\n", c);
 }
-// Should also test that insert node works for when there's no elements in the tree
+void testCountIf() {
+     int i= 0;
+    int c;
+
+    treelink t = NULL;
+    printf("\n## Test %d: 0 even and 0 odd node tree\n", ++i);
+    c = countIf(t,isEven);
+    assert(c == 0);
+    c = countIf(t,isOdd);
+    assert(c == 0);
+
+    printf("%d even nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 1 even node tree\n", ++i);
+    t = searchInsert(NULL, 10);
+    c = countIf(t,isEven);
+    assert(c == 1);
+    c = countIf(t,isOdd);
+    assert(c == 0);
+    printf("%d even nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 3 even nodes tree\n", ++i);
+    searchInsert(t, 4);
+    searchInsert(t, 12);
+    c = countIf(t,isEven);
+    assert(c == 3);
+    c = countIf(t,isOdd);
+    assert(c == 0);
+    printf("%d even nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 4 even nodes tree\n", ++i);
+    searchInsert(t, 2);
+    searchInsert(t, 7);
+    c = countIf(t, isEven);
+    assert(c == 4);
+    c = countIf(t, isOdd);
+    assert(c == 1);
+    printf("%d even nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 5 even nodes tree\n", ++i);
+    searchInsert(t, 11);
+    searchInsert(t, 18);
+    c = countIf(t, isEven);
+    assert(c == 5);
+    c = countIf(t, isOdd);
+    assert(c == 2)
+    printf("%d even nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 9 even nodes tree\n", ++i);
+    searchInsert(t, 1);
+    searchInsert(t, 3);
+    searchInsert(t, 6);
+    searchInsert(t, 8);
+    searchInsert(t, 14);
+    searchInsert(t, 26);
+    
+    c = countIf(t, isEven);
+    assert(c == 9);
+    c = countIf(t, isOdd);
+    assert(c == 4);
+    printf("%d even nodes tree\n ## passed\n", c);
+
+
+    printf("\n## Test %d: 0 negative node tree\n", ++i);
+    c = countIf(t,isNegative);
+    assert(c == 0);
+    printf("%d negative nodes tree\n ## passed\n", c);
+    
+    printf("\n## Test %d: 1 negative node tree\n", ++i);
+    t = insertTreeNode(NULL, 10);
+    c = countIf(t,isNegative);
+    assert(c == 1);
+    printf("%d negative nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 3 negative nodes tree\n", ++i);
+    insertTreeNode(t, -4);
+    insertTreeNode(t, -12);
+    c = countIf(t,isNegative);
+    assert(c == 3);
+    printf("%d negative nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 4 negative nodes tree\n", ++i);
+    insertTreeNode(t, -2);
+    insertTreeNode(t, -7);
+    c = countIf(t, -isNegative);
+    assert(c == 4);
+    printf("%d negative nodes tree\n ## passed\n", c);
+    
+    printf("\n## Test %d: 5 negative nodes tree\n", ++i);
+    insertTreeNode(t, -11);
+    insertTreeNode(t, -18);
+    c = countIf(t, -isNegative);
+    assert(c == 5);
+    printf("%d negative nodes tree\n ## passed\n", c);
+    
+    
+    printf("\n## Test %d: 9 negative nodes tree\n", ++i);
+    insertTreeNode(t, -1);
+    insertTreeNode(t, -3);
+    insertTreeNode(t, -6);
+    insertTreeNode(t, -8);
+    insertTreeNode(t, -14);
+    insertTreeNode(t, -26);
+    
+    c = countIf(t, -isNegative);
+    assert(c == 9);
+    printf("%d negative nodes tree\n ## passed\n", c);
+}
+
+void testSearchInsert(){
+//    test searching for 0
+    int i= 0;
+    int c;
+    
+    printf("%d even nodes tree\n ## passed\n", c);
+    
+    printf("\n## Test %d: 1 even node tree\n", ++i);
+    t = searchInsert(NULL, 10);
+    c = size(t);
+    assert(c == 1);
+    printf("\n ## passed\n", c);
+
+    printf("\n## Test %d: 3 even nodes tree\n", ++i);
+    searchInsert(t, 4);
+    searchInsert(t, 12);
+    c = countIf(t,isEven);
+    assert(c == 3);
+    c = countIf(t,isOdd);
+    assert(c == 0);
+    printf("%d even nodes tree\n ## passed\n", c);
+
+    printf("\n## Test %d: 4 even nodes tree\n", ++i);
+    searchInsert(t, 2);
+    searchInsert(t, 7);
+    c = countIf(t, isEven);
+    assert(c == 4);
+    c = countIf(t, isOdd);
+    assert(c == 1);
+    printf("%d even nodes tree\n ## passed\n", c);
+    
+    printf("\n## Test %d: 5 even nodes tree\n", ++i);
+    searchInsert(t, 11);
+    searchInsert(t, 18);
+    c = countIf(t, isEven);
+    assert(c == 5);
+    c = countIf(t, isOdd);
+    assert(c == 2)
+    printf("%d even nodes tree\n ## passed\n", c);
+    
+    
+    printf("\n## Test %d: 9 even nodes tree\n", ++i);
+    searchInsert(t, 1);
+    searchInsert(t, 3);
+    searchInsert(t, 6);
+    searchInsert(t, 8);
+    searchInsert(t, 14);
+    searchInsert(t, 26);
+    
+    c = countIf(t, isEven);
+    assert(c == 9);
+    c = countIf(t, isOdd);
+    assert(c == 4);
+    printf("%d even nodes
+}
 
