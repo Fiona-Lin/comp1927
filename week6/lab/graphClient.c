bc1e4f8861ee9a32811a7f12be5ee7ac281c9897
FionaCLin <clin4614@uni.sydney.edu.au>
2017-01-22 16:04:48 +1100

    back up lab 6

--- week6/lab/graphClient.c
+++ week6/lab/graphClient.c
@@ -8,6 +8,9 @@
 
 #define MAXLINE 100
 
+void testdfs(Graph graph) ;
+void testbfs(Graph graph) ;
+void testfindshortest(Graph graph) ;
 void usage(void);
 
 int randV(Graph g){
@@ -51,18 +54,18 @@ Graph graphScan(char * edgeFile){
 
 
 int main(int argc, char * argv[]){
-    
+
     srand(time(NULL));
 
     Graph graph;
     int N; char *edgeFile;
     // collect command-line params
     if (argc < 2) usage();
-    if (isdigit(argv[1][0])){ 
+    if (isdigit(argv[1][0])){
         N = atoi(argv[1]); edgeFile = NULL;
-         
-    } else { 
-        N = 0; edgeFile = argv[1]; 
+
+    } else {
+        N = 0; edgeFile = argv[1];
     }
 
     //build graph
@@ -71,18 +74,76 @@ int main(int argc, char * argv[]){
     else {
         graph = graphScan(edgeFile);
     }
-    
+
     showGraph(graph);
     // ADD Code to test graph search algorithms
-    
+
     printf("\n");
+    testdfs(graph);
+    testbfs(graph);
+    testfindshortest(graph);
     destroyGraph(graph);
-   
+
     return 0;
 }
 
-void usage()
-{
+void usage() {
         fprintf(stderr, "Usage: glab #Vertices [EdgeFile]\n");
         exit(1);
+}
+
+void testdfs(Graph graph) {
+    int i = 0;
+
+    printf("------------------------------------------------------");
+    printf("\n>>>>> Test void dfs(Graph) <<<<<\n");
+    printf("------------------------------------------------------");
+
+    // copy empty graph
+    printf("\n## Test %d: empty graph\n",++i);
+    dfSearchIterative(NULL);
+    printf("\nExpected output:\nThe graph g can't be NULL\n");
+    printf("\n ## passed ##\n");
+
+    printf("\n## Test %d:\n",++i);
+    dfSearchIterative(graph);
+
+    printf("\n ## passed ##\n");
+}
+void testbfs(Graph graph) {
+    int i = 0;
+
+    printf("------------------------------------------------------");
+    printf("\n>>>>> Test void bfs(Graph) <<<<<\n");
+    printf("------------------------------------------------------");
+
+    // copy empty graph
+    printf("\n## Test %d: empty graph\n",++i);
+    bfSearch(NULL);
+    printf("\nExpected output:\nThe graph g can't be NULL\n");
+    printf("\n ## passed ##\n");
+
+    printf("\n## Test %d:\n",++i);
+    bfSearch(graph);
+
+    printf("\n ## passed ##\n");
+}
+void testfindshortest(Graph graph) {
+    int i = 0;
+
+    printf("------------------------------------------------------");
+    printf("\n>>>>> Test void findShortestPath(Graph) <<<<<\n");
+    printf("------------------------------------------------------");
+
+    // copy empty graph
+    printf("\n## Test %d: empty graph\n",++i);
+    findShortestPath(NULL, 1, 0);
+    printf("\nExpected output:\nThe graph g can't be NULL\n");
+    printf("\n ## passed ##\n");
+
+    printf("\n## Test %d:\n",++i);
+    findShortestPath(graph, 1, 0);
+    findShortestPath(graph, 0, 2);
+
+    printf("\n ## passed ##\n");
 }
